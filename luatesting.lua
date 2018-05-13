ffaae9b763c80c064ec0fd36562ca4b87a4f1ea5
Unknown <juandibp@gmail.com>
2018-05-13 00:35:57 -0600

    Added code to sort linked data

--- luatesting.lua
+++ luatesting.lua
@@ -5,6 +5,11 @@ function setup()
 	dataFrame = readCSV("data/walmartSales.csv", ",")
 	vendedores = getCol(dataFrame,"Vendedor")
 	precios = getCol(dataFrame, "Precios")
+	local data = linkData(vendedores,precios)
+	local result = relevanceFactor(data)
+	print(#data)
+	print(#result)
+	tprint(result)
 end
 
 function draw()
@@ -18,11 +23,28 @@ function linkData(data1,data2)
 		local sq={id=data1[i],pair=data2[i]}
 		table.insert(_table,sq)
 	end
+	return _table
 end
 
-function relevanceFactor(pTable, minValueColor, maxValueColor)
 
+function by_ID(data1, data2)
+	return data1.id < data2.id -- compara por id (1er parametro de la tabla)
+end
+
+function relevanceFactor(pTable)
+	table.sort(pTable, by_ID)
 	-- se ordenan los datos de acuerdo a cierto criterio
-
-	return {table=pTable, minColor=minValueColor, maxcolor=maxValueColor}	
+	return pTable
 end
+
+function tprint (tbl, indent)
+	if not indent then indent = 0 end
+	for k, v in pairs(tbl) do
+	  formatting = string.rep("  ", indent) .. k .. ": "
+	  if type(v) == "table" then
+		tprint(v, indent+1)
+	  else
+		print(formatting .. v)
+	  end
+	end
+  end
