92ddc28886eb29a44b748aade468b390b92add90
yelizondo <yubeg971@gmail.com>
2018-05-13 00:01:23 -0600

    Updated

--- MDViz.lua
+++ MDViz.lua
@@ -82,12 +82,7 @@ function linkData(data1,data2)
 	end
 end
 
-function relevanceFactor(pTable, minValueColor, maxValueColor)
 
-	-- se ordenan los datos de acuerdo a cierto criterio
-
-	return {table=pTable, minColor=minValueColor, maxcolor=maxValueColor}	
-end
 
 --[[
 	NO OLVIDAR MANEJAR LOS COLORES
--- luatesting.lua
+++ luatesting.lua
@@ -10,3 +10,19 @@ end
 function draw()
 	background("#27274F")
 end
+
+function linkData(data1,data2)
+	local _table={}
+	for i=1,#data1
+	do
+		local sq={id=data1[i],pair=data2[i]}
+		table.insert(_table,sq)
+	end
+end
+
+function relevanceFactor(pTable, minValueColor, maxValueColor)
+
+	-- se ordenan los datos de acuerdo a cierto criterio
+
+	return {table=pTable, minColor=minValueColor, maxcolor=maxValueColor}	
+end
