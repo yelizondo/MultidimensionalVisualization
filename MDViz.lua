fc91fed73c7204dcd6d965380bb5aba68d7ec9a2
Unknown <juandibp@gmail.com>
2018-05-29 17:05:42 -0600

    No funka

--- MDViz.lua
+++ MDViz.lua
@@ -173,11 +173,11 @@ function drawPixels(width, height)
     end
 
     -- Aqui se usan esos colores para hacer los rangos y pintar los pixeles
-
+	local juandi=color(0,255,0,255)
 	for i=1, #pixels do
 		local x = pixels[i].x
 		local y = pixels[i].y
-		px[x*nrows+y] = 255
+		px[x*nrows+y] = juandi
 	end
 
 	updatePixels(grid,px)
